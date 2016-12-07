ca8e8d62a642c83401166c56754f5ab0a577dbbd
aoioandix <aoioandix@users.noreply.github.com>
2016-12-07 10:07:12 -0800

    Add files via upload
    basic code

---
+++ roguelike2/roguelike1/Core/DungeonMap.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RLNET;
+using RogueSharp;
+
+namespace roguelike1
+{
+
+    // Our custom DungeonMap class extends the base RogueSharp Map class
+    public class DungeonMap : Map
+    {
+        public List<Rectangle> Rooms;
+
+        public DungeonMap()
+        {
+            // Initialize the list of rooms when we create a new DungeonMap
+            Rooms = new List<Rectangle>();
+        }
+        // This method will be called any time we move the player to update field-of-view
+        public void UpdatePlayerFieldOfView()
+        {
+            Core.Player player = Game.Player;
+            // Compute the field-of-view based on the player's location and awareness
+            ComputeFov(player.X, player.Y, player.Awareness, true);
+            // Mark all cells in field-of-view as having been explored
+            foreach (Cell cell in GetAllCells())
+            {
+                if (IsInFov(cell.X, cell.Y))
+                {
+                    SetCellProperties(cell.X, cell.Y, cell.IsTransparent, cell.IsWalkable, true);
+                }
+            }
+        }
+        // The Draw method will be called each time the map is updated
+        // It will render all of the symbols/colors for each cell to the map sub console
+        public void Draw(RLConsole mapConsole)
+        {
+            mapConsole.Clear();
+            foreach (Cell cell in GetAllCells())
+            {
+                SetConsoleSymbolForCell(mapConsole, cell);
+            }
+        }
+
+        private void SetConsoleSymbolForCell(RLConsole console, Cell cell)
+        {
+            // When we haven't explored a cell yet, we don't want to draw anything
+            if (!cell.IsExplored)
+            {
+                return;
+            }
+
+            // When a cell is currently in the field-of-view it should be drawn with ligher colors
+            if (IsInFov(cell.X, cell.Y))
+            {
+                // Choose the symbol to draw based on if the cell is walkable or not
+                // '.' for floor and '#' for walls
+                if (cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Core.Colors.FloorFov, Core.Colors.FloorBackgroundFov, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, Core.Colors.WallFov, Core.Colors.WallBackgroundFov, '#');
+                }
+            }
+            // When a cell is outside of the field of view draw it with darker colors
+            else
+            {
+                if (cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Core.Colors.Floor, Core.Colors.FloorBackground, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, Core.Colors.Wall, Core.Colors.WallBackground, '#');
+                }
+            }
+        }
+
+        // Called by MapGenerator after we generate a new map to add the player to the map
+        public void AddPlayer(Core.Player player)
+        {
+            game.Player = player;
+            SetIsWalkable(player.X, player.Y, false);
+            UpdatePlayerFieldOfView();
+        }
+
+    }
+
+}
